3c93c693147bc0c1cd8fbba36cabea3e55325991
rafiadipramana <rafiadipramana@gmail.com>
2023-07-08 13:09:54 +0700

    Added firebase packages and change main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/home/chatroom_screen.dart';
@@ -13,7 +14,10 @@ import 'screens/sign_up_screen.dart';
 import 'screens/slide_screen.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
