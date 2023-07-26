eaaadc45c9f23ef1f016f1377da267819cc7cae1
notsatria <buanasatriaa@gmail.com>
2023-07-26 17:15:09 +0700

    Added seller detail order

---
+++ lib/screens/seller/order/seller_order_detail_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../../constant/theme.dart';
+
+class SellerOrderDetailScreen extends StatelessWidget {
+  static const routeName = '/seller-order-detail-screen';
+  const SellerOrderDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget appBar() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        title: Text(
+          'Detail Pesananku',
+          style: TextStyle(
+            color: primaryTextColor,
+            fontWeight: semiBold,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: primaryTextColor,
+          ),
+        ),
+        elevation: 2,
+      );
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: appBar(),
+        body: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+        ),
+      ),
+    );
+  }
+}
