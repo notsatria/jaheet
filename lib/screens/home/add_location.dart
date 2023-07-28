8883eecf433c7dfdb3a8ddeeeef362ce1567e597
rafiadipramana <rafiadipramana@gmail.com>
2023-07-28 16:02:41 +0700

    Add some controller

--- lib/screens/home/add_location.dart
+++ lib/screens/home/add_location.dart
@@ -3,10 +3,28 @@ import 'package:flutter/material.dart';
 import '../../constant/theme.dart';
 import 'main_screen.dart';
 
-class AddLocationScreen extends StatelessWidget {
+class AddLocationScreen extends StatefulWidget {
   const AddLocationScreen({super.key});
 
   static const routeName = "/add-location-dart";
+
+  @override
+  State<AddLocationScreen> createState() => _AddLocationScreenState();
+}
+
+class _AddLocationScreenState extends State<AddLocationScreen> {
+  TextEditingController _tipeTokoController = TextEditingController();
+  TextEditingController _namaPenerimaController = TextEditingController();
+  TextEditingController _ = TextEditingController();
+  TextEditingController kotaTokoController = TextEditingController();
+  TextEditingController provinsiTokoController = TextEditingController();
+  TextEditingController alamatTokoController = TextEditingController();
+  TextEditingController deskripsiTokoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +51,7 @@ class AddLocationScreen extends StatelessWidget {
       );
     }
 
-    Widget namaTokoForm() {
+    Widget tipeLokasi() {
       return Container(
         margin: const EdgeInsets.only(bottom: 10),
         padding: const EdgeInsets.symmetric(
@@ -50,7 +68,7 @@ class AddLocationScreen extends StatelessWidget {
             Expanded(
               child: TextFormField(
                 decoration: InputDecoration.collapsed(
-                  hintText: 'Nama Toko',
+                  hintText: 'Misal Kos, Rumah, Gedung, Apartemen',
                   hintStyle: TextStyle(
                     color: subtitleTextColor,
                   ),
@@ -66,7 +84,7 @@ class AddLocationScreen extends StatelessWidget {
       return Container(
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         child: Column(
-          children: [namaTokoForm(), namaTokoForm()],
+          children: [tipeLokasi()],
         ),
       );
     }
