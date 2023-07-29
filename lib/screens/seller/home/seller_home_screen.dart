7a1f4b2c44f75baf1aa388bb931e143c47ce57ef
notsatria <buanasatriaa@gmail.com>
2023-07-29 17:41:36 +0700

    Added dashboard screen

--- lib/screens/seller/home/seller_home_screen.dart
+++ lib/screens/seller/home/seller_home_screen.dart
@@ -1,10 +1,240 @@
 import 'package:flutter/material.dart';
+import 'package:jahitin/constant/theme.dart';
 
 class SellerHomeScreen extends StatelessWidget {
   const SellerHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    Widget helloSeller() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: Text(
+          'Hello, Penjaheet!',
+          style: primaryTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: bold,
+          ),
+        ),
+      );
+    }
+
+    Widget cardPesananMasuk() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 10,
+              spreadRadius: 5,
+              offset: const Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pesanan Masuk',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '1',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Potensi',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      '+Rp 100.000',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Icon(
+              Icons.bar_chart_rounded,
+              size: 75,
+              color: secondaryColor,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget cardPesananSelesai() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 10,
+              spreadRadius: 5,
+              offset: const Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pesanan Selesai',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '10',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Keuntungan:',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Rp 1.000.000',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Icon(
+              Icons.monetization_on_rounded,
+              size: 75,
+              color: secondaryColor,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget cardPesananDiproses() {
+      return Container(
+        margin: const EdgeInsets.only(top: 20),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              blurRadius: 10,
+              spreadRadius: 5,
+              offset: const Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pesanan Dalam Proses',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 18,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '4',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Segera Selesaikan',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Pesanan Anda',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 16,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Icon(
+              Icons.pending_actions_rounded,
+              size: 75,
+              color: secondaryColor,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              helloSeller(),
+              cardPesananMasuk(),
+              cardPesananSelesai(),
+              cardPesananDiproses(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
